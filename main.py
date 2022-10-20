48471162d741a9535dbe6d0b8fd563a3c334714d
SzuSa <116305118+SzuSa@users.noreply.github.com>
2022-10-20 22:24:52 +0200

    Adding multiple targets

--- main.py
+++ main.py
@@ -5,21 +5,42 @@ def on_button_pressed_a():
     basic.clear_screen()
     button_action = 1
     basic.pause(100)
-    level = level - 1
+    level += -1
     speed = speed_list[level]
     basic.clear_screen()
-    basic.show_string("" + str((level)))
+    basic.show_string("" + str(level))
     basic.pause(500)
     basic.clear_screen()
     button_action = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global target_counter, x1, y1, x2, y2, x3, y3, button_action
+    if target_counter == 3:
+        target_counter = 0
+    x1 = 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
+    x3 = 0
+    y3 = 0
+    basic.clear_screen()
+    button_action = 1
+    target_counter += 1
+    basic.pause(100)
+    basic.clear_screen()
+    basic.show_string("" + str(target_counter))
+    basic.pause(500)
+    basic.clear_screen()
+    button_action = 0
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global button_action
     basic.clear_screen()
     button_action = 1
     basic.pause(100)
-    if x == 2 and y == 2:
+    if x1 == 2 and y1 == 2 or x2 == 2 and y2 == 2 or x3 == 2 and y3 == 2:
         for index in range(3):
             basic.clear_screen()
             basic.show_icon(IconNames.SMALL_HEART)
@@ -36,8 +57,13 @@ def on_button_pressed_b():
     button_action = 0
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-y = 0
-x = 0
+y3 = 0
+x3 = 0
+y2 = 0
+x2 = 0
+y1 = 0
+x1 = 0
+target_counter = 0
 speed = 0
 level = 0
 speed_list: List[number] = []
@@ -48,11 +74,31 @@ level = 4
 speed = speed_list[level]
 
 def on_forever():
-    global x, y
-    if button_action != 1:
-        x = randint(0, 4)
-        y = randint(0, 4)
-        led.plot(x, y)
+    global x1, y1
+    if button_action != 1 and target_counter >= 1:
+        x1 = randint(0, 4)
+        y1 = randint(0, 4)
+        led.plot(x1, y1)
         basic.pause(speed)
-        led.unplot(x, y)
+        led.unplot(x1, y1)
 basic.forever(on_forever)
+
+def on_forever2():
+    global x2, y2
+    if button_action != 1 and target_counter >= 2:
+        x2 = randint(0, 4)
+        y2 = randint(0, 4)
+        led.plot(x2, y2)
+        basic.pause(speed)
+        led.unplot(x2, y2)
+basic.forever(on_forever2)
+
+def on_forever3():
+    global x3, y3
+    if button_action != 1 and target_counter >= 3:
+        x3 = randint(0, 4)
+        y3 = randint(0, 4)
+        led.plot(x3, y3)
+        basic.pause(speed)
+        led.unplot(x3, y3)
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,26 +1,43 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     if (level == 0) {
         level = 5
     }
-    
     basic.clearScreen()
     button_action = 1
     basic.pause(100)
-    level = level - 1
+    level += -1
     speed = speed_list[level]
     basic.clearScreen()
-    basic.showString("" + ("" + level))
+    basic.showString("" + level)
     basic.pause(500)
     basic.clearScreen()
     button_action = 0
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+input.onButtonPressed(Button.AB, function () {
+    if (target_counter == 3) {
+        target_counter = 0
+    }
+    x1 = 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
+    x3 = 0
+    y3 = 0
+    basic.clearScreen()
+    button_action = 1
+    target_counter += 1
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showString("" + target_counter)
+    basic.pause(500)
+    basic.clearScreen()
+    button_action = 0
+})
+input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     button_action = 1
     basic.pause(100)
-    if (x == 2 && y == 2) {
+    if (x1 == 2 && y1 == 2 || x2 == 2 && y2 == 2 || x3 == 2 && y3 == 2) {
         for (let index = 0; index < 3; index++) {
             basic.clearScreen()
             basic.showIcon(IconNames.SmallHeart)
@@ -29,35 +46,61 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
             basic.pause(100)
         }
     } else {
-        for (let index2 = 0; index2 < 1; index2++) {
+        for (let index = 0; index < 1; index++) {
             basic.showIcon(IconNames.No)
             basic.pause(500)
             basic.clearScreen()
             basic.pause(500)
         }
     }
-    
     basic.clearScreen()
     button_action = 0
 })
-let y = 0
-let x = 0
+let y3 = 0
+let x3 = 0
+let y2 = 0
+let x2 = 0
+let y1 = 0
+let x1 = 0
+let target_counter = 0
 let speed = 0
 let level = 0
-let speed_list : number[] = []
+let speed_list: number[] = []
 let button_action = 0
 button_action = 0
-speed_list = [100, 200, 500, 1000, 1500]
+speed_list = [
+100,
+200,
+500,
+1000,
+1500
+]
 level = 4
 speed = speed_list[level]
-basic.forever(function on_forever() {
-    
-    if (button_action != 1) {
-        x = randint(0, 4)
-        y = randint(0, 4)
-        led.plot(x, y)
+basic.forever(function () {
+    if (button_action != 1 && target_counter >= 1) {
+        x1 = randint(0, 4)
+        y1 = randint(0, 4)
+        led.plot(x1, y1)
         basic.pause(speed)
-        led.unplot(x, y)
+        led.unplot(x1, y1)
     }
-    
+})
+basic.forever(function () {
+    if (button_action != 1 && target_counter >= 2) {
+        x2 = randint(0, 4)
+        y2 = randint(0, 4)
+        led.plot(x2, y2)
+        basic.pause(speed)
+        led.unplot(x2, y2)
+    }
+})
+basic.forever(function () {
+    if (button_action != 1 && target_counter >= 3) {
+        x3 = randint(0, 4)
+        y3 = randint(0, 4)
+        led.plot(x3, y3)
+        basic.pause(speed)
+        led.unplot(x3, y3)
+    }
 })
